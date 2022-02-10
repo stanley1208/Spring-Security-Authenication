b6f4fe5584ac1dcd18a31460e564c1ce3abbe606
stanley1208 <stanley.mei08@gmail.com>
2022-02-10 15:55:58 +0800

    java

--- src/main/java/com/example/demo/repository/UserDao.java
+++ src/main/java/com/example/demo/repository/UserDao.java
@@ -13,8 +13,8 @@ public class UserDao {
 		// A01, 1234 ,admin,normal,ROLE_manager
 		Map<String, String>info1=new LinkedHashMap<>();
 		info1.put("password", "$2a$10$TrgQ4ZDsiLWmF21dmO1kUuzlnIf72wSpm95WUtExjd8StL3yDHW1u");
-		info1.put("authority", "admin,normal,ROLE_manager");
-		// A02, 5678, 
+		info1.put("authority", ",normal,ROLE_manager");
+		// A02, 5678, admin
 		Map<String, String>info2=new LinkedHashMap<>();
 		info2.put("password", "$2a$10$k2d3Mo9kOu0P8ooX1rAFNeA3Hyk8R7H5aZxd1QBqoXds4F1rppH..");
 		info2.put("authority", "normal,ROLE_employee");
